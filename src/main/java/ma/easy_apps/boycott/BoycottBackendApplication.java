0b7697e5c99a8ec71daa3af32c760f92b1da5477
HASSANELMALLOUKY <hassan.elmallouky@gmail.com>
2025-07-30 14:12:03 +0100

    init

---
+++ src/main/java/ma/easy_apps/boycott/BoycottBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.easy_apps.boycott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoycottBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoycottBackendApplication.class, args);
+    }
+
+}
